b0a6404a9678875c0eaecd9e2fa48dda70ecf27a
julie <julie.da-prato@live.fr>
2018-11-18 20:58:52 +0100

    part 3 dump still not working

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -10,19 +10,30 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+    private $repository;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index(): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Property::class);
+
+        $property = $this->repository->findAllVisible();
+        $repo = $this->getDoctrine()->getRepository($property);
         dump($repo);
         return $this->render("property/index.html.twig", [
             'current_menu' => 'properties',
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -19,32 +19,40 @@ class PropertyRepository extends ServiceEntityRepository
         parent::__construct($registry, Property::class);
     }
 
+    public function findAllVisible()
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.sold = false')
+            ->getQuery()
+            ->getResult();
+    }
+
     // /**
     //  * @return Property[] Returns an array of Property objects
     //  */
     /*
     public function findByExampleField($value)
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+    return $this->createQueryBuilder('p')
+    ->andWhere('p.exampleField = :val')
+    ->setParameter('val', $value)
+    ->orderBy('p.id', 'ASC')
+    ->setMaxResults(10)
+    ->getQuery()
+    ->getResult()
+    ;
     }
-    */
+     */
 
     /*
-    public function findOneBySomeField($value): ?Property
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+public function findOneBySomeField($value): ?Property
+{
+return $this->createQueryBuilder('p')
+->andWhere('p.exampleField = :val')
+->setParameter('val', $value)
+->getQuery()
+->getOneOrNullResult()
+;
+}
+ */
 }
